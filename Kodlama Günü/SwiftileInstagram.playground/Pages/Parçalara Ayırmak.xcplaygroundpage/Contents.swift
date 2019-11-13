2fa04d46d558c9e5fbb613f2a7e58bc75ec4028b
Emirhan Erdogan <emirerdo@yandex.com>
2019-11-13 14:17:20 +0300

    final

--- Kodlama Günü/SwiftileInstagram.playground/Pages/Parçalara Ayırmak.xcplaygroundpage/Contents.swift
+++
@@ -1,9 +0,0 @@
-//: ## Parçalara Ayırmak
-//: Geliştiricilerin iyi oldukları konulardan biri de büyük ve karmaşık yapıları küçük ve anlaşılması kolay parçalara ayırarak sadeleştirmektir.
-//: Bir Instagram gönderisini tasarlamak ve kodlamak, başta karmaşık gibi görünebilir. Fakat gönderiyi oluşturan parçaları düşünerek ve onları sırayla geliştirerek karmaşayı eğlenceye çevirmek sizin elinizde!
-//:
-//: ![Header View](instagram-parts.pdf)
-//:
-//:\
-//: Bir sonraki sayfada Instagram gönderisinin ilk parçası olan _HeaderView_'ı tanıyalım.\
-//:[Geri](@previous)  |  Sayfa 2 / 9  |  [İleri: Header Nedir?](@next)
